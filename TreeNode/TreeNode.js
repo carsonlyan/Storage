03f1245cdc8ad610cb6b345d3f6be35315310e01
CHEN Qun <qun.chen@hexagon.com>
2021-10-28 17:08:46 +0800

    Make search bar and tree nodes impacted with each other.

--- TreeNode/TreeNode.js
+++ TreeNode/TreeNode.js
@@ -1,10 +1,11 @@
-import React, { Component, useEffect, useReducer, useState } from 'react';
-import Checkbox from '@mui/material/Checkbox';
+import React, { useCallback, useEffect, useState } from 'react';
 import TreeView from '@mui/lab/TreeView';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import TreeItem from '@mui/lab/TreeItem';
-import { domains } from './fakeData';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 const checkMap = {
   2: 'checked',
@@ -12,117 +13,106 @@ const checkMap = {
   0: ''
 }
 
-const getMockData = () => {
-  let resData = JSON.parse(JSON.stringify(domains.domains));
-  const travelChildren = (domain, index) => {
-    domain.children.forEach((childDomain, childIndex) => {
-      childDomain.id = index.toString() + childIndex.toString();
-      childDomain.checked = 0;
-      travelChildren(childDomain, index.toString() + childIndex.toString());
-    })
-  }
-  resData.forEach((domain, index) => {
-    domain.id = index.toString();
-    domain.checked = 0;
-    travelChildren(domain, index);
-  })
-  return resData;
-};
-
-const mockData = getMockData();
-
-// const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
-
-const TreeNode = ({ label, id, checked, onSelect }) => {
+const TreeNode = ({ label, id, checked, onCheck }) => {
   return (
-      <div>
-          <Checkbox id={id} checked={checked? true: false} onChange={(e) => onSelect(e)} />
-          <label>{label}</label>
-      </div>
+    <FormGroup>
+      <FormControlLabel control={<Checkbox id={id} checked={checked===2? true:false} indeterminate={checked===1? true:false} onChange={(e) => onCheck(e)} />} label={label} />
+    </FormGroup>
   )
 }
 
-export function TreeNodeComp() {
-  const [treeData, setTreeData] = useState(mockData);
-  const [selectedId, setSelectedId] = useState([]);
+export function TreeNodeComp({ data, checkedNodeIDs, onCheck }) {
+  const [treeData, setTreeData] = useState(data);
+
+  const updateChildrenNodes = useCallback(
+    (node, isChecked) => {
+      node.children.forEach((childNode) => {
+        childNode.checked = isChecked;
+        updateChildrenNodes(childNode, isChecked);
+      })
+    }, []);
   
-  const updateSelectedNode = (tree_data, id, isChecked, deepth) => {
-    const parentId = id.slice(0, -1);
-    function findParentNode (node) {
-      for(const childNode of node) {
-        if(childNode.id === parentId) return childNode;
-        else {
-          const res = findParentNode(childNode.children);
-          if(res) return res;
+  const updateCheckedNodes = useCallback(
+    (tree_data, id, isChecked, deepth) => {
+      const parentId = id.slice(0, -1);
+      function findParentNode (node) {
+        for(const childNode of node) {
+          if(childNode.id === parentId) return childNode;
+          else {
+            const res = findParentNode(childNode.children);
+            if(res) return res;
+          }
         }
       }
-    }
-    if(!parentId) {
-      const node = tree_data.find((node) => node.id === id);
-      node.checked = isChecked;
-      if(deepth === 0) updateChildrenNodes(node, isChecked);
-      return;
-    } 
-    const parentNode = findParentNode(tree_data);
-    const childrenNodes = parentNode.children;
-    if(deepth === 0) {
-      const node = childrenNodes.find((node) => node.id === id);
-      updateChildrenNodes(node, isChecked);
-    }
-    if(isChecked) {
-      let allChildrenChecked = true;
-      childrenNodes.forEach((node) => {
-        if (node.id === id) node.checked = isChecked;
-        else if(node.checked !== 2) allChildrenChecked = false;
-      })
-      if (allChildrenChecked) parentNode.checked = 2;
-      else parentNode.checked = 1;
-    } else {
-      let allChildrenNotChecked = true;
-      childrenNodes.forEach((node) => {
-        if(node.id === id) node.checked = isChecked;
-        else if(node.checked !== 0) allChildrenNotChecked = false;
-      })
-      if (allChildrenNotChecked) parentNode.checked = 0;
-      else parentNode.checked = 1;
-    }
-    updateSelectedNode(tree_data, parentId, parentNode.checked, ++deepth);
-  }
+      if(!parentId) {
+        const node = tree_data.find((node) => node.id === id);
+        node.checked = isChecked;
+        if(deepth === 0) updateChildrenNodes(node, isChecked);
+        return;
+      } 
+      const parentNode = findParentNode(tree_data);
+      const childrenNodes = parentNode.children;
+      if(deepth === 0) {
+        const node = childrenNodes.find((node) => node.id === id);
+        updateChildrenNodes(node, isChecked);
+      }
+      if(isChecked) {
+        let allChildrenChecked = true;
+        childrenNodes.forEach((node) => {
+          if (node.id === id) node.checked = isChecked;
+          else if(node.checked !== 2) allChildrenChecked = false;
+        })
+        if (allChildrenChecked) parentNode.checked = 2;
+        else parentNode.checked = 1;
+      } else {
+        let allChildrenNotChecked = true;
+        childrenNodes.forEach((node) => {
+          if(node.id === id) node.checked = isChecked;
+          else if(node.checked !== 0) allChildrenNotChecked = false;
+        })
+        if (allChildrenNotChecked) parentNode.checked = 0;
+        else parentNode.checked = 1;
+      }
+      updateCheckedNodes(tree_data, parentId, parentNode.checked, ++deepth);
+    }, [updateChildrenNodes])
 
-  const updateChildrenNodes = (node, isChecked) => {
-    node.children.forEach((childNode) => {
-      childNode.checked = isChecked;
-      updateChildrenNodes(childNode, isChecked);
-    })
-  }
-
-  // const findSelectedNode = (tree_data, id) => {
-  //   for(const childNode of tree_data) {
-  //     if(childNode.id === id) return childNode;
-  //     else {
-  //       const res = findSelectedNode(childNode.children, id);
-  //       if(res) return res;
-  //     }
-  //   }
-  // }
-
-  const handleOnSelect = (event) => {
-    const id = event.target.id;
-    const isChecked = event.target.checked;
-    const tempTreeData = JSON.parse(JSON.stringify(treeData));
-
+  useEffect(() => {
     setTreeData((prevTreeData) => {
       const tempTreeData = JSON.parse(JSON.stringify(prevTreeData));
-      updateSelectedNode(tempTreeData, id, isChecked?2:0, 0);
+      checkedNodeIDs.forEach((id) => updateCheckedNodes(tempTreeData, id, 2, 0))
       return tempTreeData;
     })
+  }, [checkedNodeIDs, updateCheckedNodes])
+
+  const collectCheckedNodes = (nodes, checkedNodes) => {
+    nodes.forEach((node) => {
+      if (node.checked === 2) {
+        checkedNodes.push(node);
+        collectCheckedNodes(node.children, checkedNodes);
+      }
+      else collectCheckedNodes(node.children, checkedNodes);
+    })
+  }
+
+  const handleOnCheck = (event) => {
+    const id = event.target.id;
+    const isChecked = event.target.checked;
+    setTreeData((prevTreeData) => {
+      const tempTreeData = JSON.parse(JSON.stringify(prevTreeData));
+      updateCheckedNodes(tempTreeData, id, isChecked?2:0, 0);
+      const checkedNodes = [];
+      collectCheckedNodes(tempTreeData, checkedNodes);
+      onCheck(checkedNodes);
+      return tempTreeData;
+    })
+
   }
 
   const renderNode = (node) => {
     const { id, checked, name } = node;
 
     return (
-      <TreeItem key={id} nodeId={id} label={<TreeNode label={name} id={id} checked={checked} onSelect={handleOnSelect} />}>
+      <TreeItem key={id} nodeId={id} label={<TreeNode label={name} id={id} checked={checked} onCheck={handleOnCheck} />}>
         {node.children.map((childNode) => {
           return renderNode(childNode);
         })}
@@ -135,29 +125,9 @@ export function TreeNodeComp() {
       aria-label="multi-select"
       defaultCollapseIcon={<ExpandMoreIcon />}
       defaultExpandIcon={<ChevronRightIcon />}
-      sx={{ height: 216, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
+      sx={{ width: '100%' }}
     >
       {treeData.map((node) => renderNode(node))}
-      {/* <TreeItem nodeId="1" label={<TreeItemContent content="Applications" />}>
-        <TreeItem nodeId="22" label="Calendar" />
-        <TreeItem nodeId="3" label="Chrome" />
-        <TreeItem nodeId="4" label="Webstorm" />
-      </TreeItem>
-      <TreeItem nodeId="5" label="Documents">
-        <TreeItem nodeId="6" label="MUI">
-          <TreeItem nodeId="7" label="src">
-            <TreeItem nodeId="8" label="index.js" />
-            <TreeItem nodeId="9" label="tree-view.js" />
-          </TreeItem>
-        </TreeItem>
-      </TreeItem> */}
     </TreeView>
   );
 }
-
-
-// export const TreeNodeComp = () => {
-//     return (
-//         <TreeItemContent content='hello' />
-//     )
-// }
